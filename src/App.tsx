e5843038606624946d96784c69a6c0f185e26dc7
Gabriel Da Silva <gabrieldasilva.itu2001@gmail.com>
2025-07-12 02:04:09 -0300

    PrivateRoute

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import AdminHome from './routes/Admin/AdminHome';
 import Admin from './routes/Admin';
  import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
  import {history} from './utils/history';
+import { PrivateRoute } from './components/PrivateRoute';
 
 export default function App() {
 
@@ -28,7 +29,7 @@ export default function App() {
          <Route path="cart" element={<Cart/>}/>
          <Route path="login" element={<Login />} />
          </Route>
-         <Route path='/admin/' element={<Admin/>}> 
+         <Route path="/admin/" element={<PrivateRoute><Admin /></PrivateRoute>} >
          <Route index element={<AdminHome/>} /> 
          </Route>
          <Route path='*' element={<Navigate to="/"/>}>  
