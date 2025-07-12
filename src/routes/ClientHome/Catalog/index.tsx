78d04b30f9edbeb83335aec8ee9d7dd0fe7bebc5
Gabriel Da Silva <gabrieldasilva.itu2001@gmail.com>
2025-07-12 18:52:33 -0300

    hasAnyRoles

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -5,7 +5,7 @@ import ButtonNextPage from '../../../components/ButtonNextPage';
 import { useEffect, useState } from 'react';
 import { ProductDTO } from '../../../models/product';
 import *as productService from '../../../services/product-service';
-import { isAuthenticated } from '../../../services/auth-service';
+import { hasAnyRoles, isAuthenticated } from '../../../services/auth-service';
 
 type QueryParams = {
     page: number;
@@ -27,6 +27,7 @@ export default function Catalog() {
 
 
         console.log("Autenticado " + isAuthenticated())
+        console.log("TESTE ", hasAnyRoles(['ROLE_CLIENT']));
         productService.findPageRequest(queryParams.page, queryParams.name)
         .then(response =>{
             const nextPage = response.data.content;
