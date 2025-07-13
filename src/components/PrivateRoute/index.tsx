bd9da75f541113d537b35c87cd3b0629d567f973
Gabriel Da Silva <gabrieldasilva.itu2001@gmail.com>
2025-07-12 23:56:13 -0300

    Access Control Route Level

--- src/components/PrivateRoute/index.tsx
+++ src/components/PrivateRoute/index.tsx
@@ -1,15 +1,19 @@
+import { RoleEnum } from '../../models/auth';
 import *as authService from '../../services/auth-service';
 import { Navigate } from "react-router-dom";
 
 
 type Props = {
-  children: JSX.Element;
-};
+    children: JSX.Element;
+    roles?: RoleEnum[];
+}
 
-
-export function PrivateRoute({ children }: Props) {
-  if (!authService.isAuthenticated()) {
-    return <Navigate to="/login" />;
-  }
-  return children;
+export function PrivateRoute({ children, roles = [] }: Props) {
+    if (!authService.isAuthenticated()) {
+        return <Navigate to="/login" />;
+    }
+    if (!authService.hasAnyRoles(roles)) {
+        return <Navigate to="/catalog" />;
+    }
+    return children;
 }
