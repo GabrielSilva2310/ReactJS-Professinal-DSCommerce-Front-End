6fe400d873340e2de9280c64e506fda141318f1c
Gabriel Da Silva <gabrieldasilva.itu2001@gmail.com>
2025-07-23 17:35:58 -0300

    Delete Finish

--- src/services/product-service.ts
+++ src/services/product-service.ts
@@ -22,3 +22,14 @@ export function findById(id: number) {
     return requestBackend({ url: `/products/${id}`})
 }
 
+
+export function deleteById(id: number) {
+    const config : AxiosRequestConfig = {
+        method: "DELETE",
+        url: `/products/${id}`,
+        withCredentials: true
+    }
+
+    return requestBackend(config);
+}
+
