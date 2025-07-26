43fd983de3ca3d97ead23b79181fee98778c3622
Gabriel Da Silva <gabrieldasilva.itu2001@gmail.com>
2025-07-26 01:57:07 -0300

    forms update

--- src/routes/ClientHome/Login/index.tsx
+++ src/routes/ClientHome/Login/index.tsx
@@ -1,6 +1,7 @@
 import { useContext, useState } from 'react';
 import './style.css';
 import *as authService from '../../../services/auth-service';
+import * as forms from '../../../utils/forms';
 import { useNavigate } from 'react-router-dom';
 import { ContextToken } from '../../../utils/context-token';
 import FormInput from '../../../components/FormInput';
@@ -49,9 +50,7 @@ export default function Login(){
   }
 
     function handleInputChange(event: any){
-    const value = event.target.value;
-    const name = event.target.name;
-    setFormData({...formData, [name]: { ...formData[name], value: value}});
+    setFormData(forms.update(formData, event.target.name, event.target.value));
 
   }
 
